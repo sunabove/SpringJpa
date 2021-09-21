037d773a9d632bba235d6f58e5127e31d4509f90
sunabove <sunabove@nate.com>
2021-09-21 10:33:04 +0900

    Update DataBaseConfig.java

--- src/main/java/com/example/demo/config/DataBaseConfig.java
+++ src/main/java/com/example/demo/config/DataBaseConfig.java
@@ -1,13 +1,7 @@
 package com.example.demo.config;
 
-import java.util.Properties;
-
-import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.*;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
 public class DataBaseConfig {
